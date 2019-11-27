ea93d44416aa98bb38c3dedffff7d6f650535ec1
Jos Kuijpers <jkuijpers@jarvix.nl>
2019-11-27 21:10:34 +0100

    Initial Commit

---
+++ ISOGame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ISOGame
+//
+//  Created by Jos Kuijpers on 27/11/2019.
+//  Copyright © 2019 Jos Kuijpers. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
