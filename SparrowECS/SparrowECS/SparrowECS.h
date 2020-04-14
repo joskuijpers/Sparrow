63db872c7271712727e19fddaf935e16aa3bf867
Jos Kuijpers <jkuijpers@jarvix.nl>
2020-04-14 22:19:30 +0200

    Add new SparrowECS package.

---
+++ SparrowECS/SparrowECS/SparrowECS.h
@@ -0,0 +1,19 @@
+//
+//  SparrowECS.h
+//  SparrowECS
+//
+//  Created by Jos Kuijpers on 14/04/2020.
+//  Copyright © 2020 Jos Kuijpers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SparrowECS.
+FOUNDATION_EXPORT double SparrowECSVersionNumber;
+
+//! Project version string for SparrowECS.
+FOUNDATION_EXPORT const unsigned char SparrowECSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SparrowECS/PublicHeader.h>
+
+
