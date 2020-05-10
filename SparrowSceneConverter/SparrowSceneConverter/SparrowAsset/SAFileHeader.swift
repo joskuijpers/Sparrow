326c8854e84238d2e5541fce156a176a20d6dc00
Jos Kuijpers <jkuijpers@jarvix.nl>
2020-05-10 23:00:51 +0200

    Improved code by using var instead of let for default values, requiring no custom initialiser.

--- SparrowSceneConverter/SparrowSceneConverter/SparrowAsset/SAFileHeader.swift
+++ SparrowSceneConverter/SparrowSceneConverter/SparrowAsset/SAFileHeader.swift
@@ -12,41 +12,27 @@ import SparrowBinaryCoder
 /// Sparrow Asset file header.
 struct SAFileHeader: BinaryCodable {
     /// Indicator of the SparrowAsset file: a prefix.
-    let indicator: SAFileHeaderIndicator
+    private(set) var indicator = SAFileHeaderIndicator() // Must be a var so codable can override it
     
     /// Version of the file format
-    let version: SAFileVersion
+    private(set) var version: SAFileVersion = .version1
     
     /// The generator used to generate the file.
-    var generator: String
+    let generator: String
     
     /// The origin of the data used to generate the file. Useful for re-generation
-    var origin: String
+    let origin: String
     
     /// The version of the asset file format
     enum SAFileVersion: UInt8, BinaryCodable {
         case version1 = 1
-    }
-    
-    init(generator: String, origin: String?) {
-        self.indicator = SAFileHeaderIndicator()
-        self.version = .version1
-        self.generator = generator
-        self.origin = origin ?? ""
     }
 }
 
 /// File header indicator: 3 bytes 'SA '
 struct SAFileHeaderIndicator: BinaryCodable {
     // A string has a size encoded which we want to ignore for this special case.
-    private let p1: UInt8
-    private let p2: UInt8
-    private let p3: UInt8
-    
-    init() {
-        // Note: cannot use default values as they are not read!
-        p1 = UInt8("S".utf8.first!)
-        p2 = UInt8("A".utf8.first!)
-        p3 = UInt8(" ".utf8.first!)
-    }
+    private var p1 = UInt8("S".utf8.first!)
+    private var p2 = UInt8("A".utf8.first!)
+    private var p3 = UInt8(" ".utf8.first!)
 }
