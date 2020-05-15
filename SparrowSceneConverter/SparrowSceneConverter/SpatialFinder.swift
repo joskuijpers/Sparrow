426a754d17d076bdaa43c1f9174f30c2dd1ac5e4
Jos Kuijpers <jkuijpers@jarvix.nl>
2020-05-15 21:47:47 +0200

    Implement spatial sort from Assimp for much faster tangent generation

--- SparrowSceneConverter/SparrowSceneConverter/SpatialFinder.swift
+++ SparrowSceneConverter/SparrowSceneConverter/SpatialFinder.swift
@@ -10,38 +10,77 @@ import simd
 
 struct SpatialFinder {
     private let positions: [float3]
-    private let planeNormal = float3(0.8523, 0.34321, 0.5736)
+    private static let planeNormal = float3(0.8523, 0.34321, 0.5736)
     
-    private var map: [Entry]
+    private var entries: [Entry]
     
     init(_ positions: [float3]) {
         self.positions = positions
         
-        map = []
-        
-        for (index, position) in positions.enumerated() {
-            let distance = dot(position, planeNormal)
-            map.append(Entry(index: index, distance: distance))
-        }
-    
-        map.sort(by: { $0.distance < $1.distance })
+        entries = positions
+            .enumerated()
+            .map { index, position in
+                Entry(index: index, distance: dot(position, SpatialFinder.planeNormal))
+            }
+            .sorted(by: { $0.distance < $1.distance })
     }
     
-    // https://github.com/assimp/assimp/blob/master/code/Common/SpatialSort.cpp#L115
+    // https://github.com/assimp/assimp/blob/master/code/Common/SpatialSort.cpp
+    /// Find any positions within the radius near given position.
     func near(_ position: float3, radius: Float) -> [Int] {
         var result: [Int] = []
         
-        // TODO: this is the simplest, worst performing implementation O(n)
-        for entry in map {
-            if distance(positions[entry.index], position) <= radius {
-                result.append(entry.index)
-            }
+        if positions.count == 0 {
+            return result
         }
         
+        let dist = dot(position, SpatialFinder.planeNormal)
+        let minDist = dist - radius
+        let maxDist = dist + radius
+        
+        if maxDist < entries.first!.distance {
+            return result
+        }
+        if minDist > entries.last!.distance {
+            return result
+        }
+        
+        // Do a binary search
+        var index = entries.count / 2
+        var stepSize = entries.count / 4
+        
+        while stepSize > 1 {
+            if entries[index].distance < minDist {
+                index += stepSize
+            } else {
+                index -= stepSize
+            }
+            
+            stepSize /= 2
+        }
+
+        // May need one more step to get to the actual index
+        while index > 0 && entries[index].distance > minDist {
+            index -= 1
+        }
+        while index < entries.count - 1 && entries[index].distance < minDist {
+            index += 1
+        }
+        
+        let squared = radius * radius
+        while index < entries.count && entries[index].distance < maxDist {
+            let entry = entries[index]
+            if length_squared(positions[entry.index] - position) < squared {
+                result.append(entry.index)
+            }
+            
+            index += 1
+        }
+
         return result
     }
     
-    struct Entry {
+    private struct Entry {
         let index: Int
         let distance: Float
     }
