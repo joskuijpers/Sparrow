99fadfd9733de8cacc744c67f0e14cd95ef14ff2
Jos Kuijpers <jkuijpers@jarvix.nl>
2020-06-05 17:31:31 +0200

    Move Light component to the library. Improved documentation. Added more physical properties. Moved textureLoader into Graphics folder

---
+++ SparrowEngine2/Sources/SparrowEngine2/Graphics/Components/Light.swift
@@ -0,0 +1,57 @@
+//
+//  Light.swift
+//  ISOGame
+//
+//  Created by Jos Kuijpers on 03/01/2020.
+//  Copyright © 2020 Jos Kuijpers. All rights reserved.
+//
+
+//import Foundation
+import SparrowECS
+
+/// Type of a light
+public enum LightType {
+    /// A light source with a uniform direction and constant intensity.
+    case directional
+    
+    /// An omnidirectional light.
+    case point
+    
+    /// A light source that illuminates a cone-shaped area.
+//    case spot
+}
+
+/// A light source.
+public final class Light: Component {
+    /// Type of the light.
+    public let type: LightType
+    
+    /// Light color.
+    ///
+    /// The actual color is determined by multiplying the `temperature` color with `color`.
+    public var color = float3.one
+    
+    /// Light direction (for spot and directional lights). // TODO; replace with rotation of transform?
+    public var direction = float3(0, -1, 0)
+    
+    /// The temperature of the light in Kelvin.
+    ///
+    /// The actual color is determined by multiplying the `temperature` color with `color`.
+    public var temperature: Int = 6500
+    
+    /// The intensity of the light in Candela
+    public var intensity: Float = 1
+//    public var intensity: Float = 1000 // LUMENS???
+    
+    
+    // spotInnerAngle, spotOuterAngle
+    // range / attenuationStartDistance, attenuationEndDistance, attenuationFalloffExponent
+    
+    //    public var castsShadow: Bool = false
+    // shadowMapSize, shadowBias
+    // zNear, zFar, orthographicScale
+    
+    public init(type: LightType) {
+        self.type = type
+    }
+}
