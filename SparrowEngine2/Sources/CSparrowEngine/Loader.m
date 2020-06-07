d0a0f8e69b794d4e2936e59ca160a270f1e12c27
Jos Kuijpers <jkuijpers@jarvix.nl>
2020-06-07 21:33:32 +0200

    Add new bounds tests

--- SparrowEngine2/Sources/CSparrowEngine/Loader.m
+++ SparrowEngine2/Sources/CSparrowEngine/Loader.m
@@ -1,1 +1,11 @@
-#import "include/ShaderShared.h"
+//
+//  Loader.m
+//  SparrowEngine
+//
+//  Created by Jos Kuijpers on 7/6/2020
+//  Copyright © 2020 Jos Kuijpers. All rights reserved.
+//
+//  This file is only here to prevent full omittance of compiling the target.
+//
+
+//#import "include/ShaderShared.h"
--- SparrowEngine2/Tests/SparrowEngine2Tests/SparrowEngine2Tests.swift
+++ SparrowEngine2/Tests/SparrowEngine2Tests/SparrowEngine2Tests.swift
@@ -6,11 +6,15 @@ final class SparrowEngine2Tests: XCTestCase {
     func testBounds() {
         let bounds = Bounds(center: [0, 1, 0], extents: [1, 1, 1])
         // -1, 0, -1   to   1, 2, 1
-        
+
         XCTAssert(bounds.contains(point: [0, 1, 0]))
         XCTAssert(bounds.contains(point: [1, 1, 1]))
         XCTAssert(bounds.contains(point: [1, 2, 1]))
         XCTAssert(bounds.contains(point: [0, 0, 0]))
+        XCTAssert(!bounds.contains(point: [0, 10, 0]))
+        XCTAssert(!bounds.contains(point: [-1, 10, 0]))
+        XCTAssert(!bounds.contains(point: [-1, -1, -1]))
+        XCTAssert(!bounds.contains(point: [-1, -1, 1]))
     }
     
     func testCameraShaderInfo() {
