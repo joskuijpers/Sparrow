a601543f53b2e6dbf35dca3f72675da17d71015c
Jos Kuijpers <jkuijpers@jarvix.nl>
2020-07-05 22:24:42 +0200

    Change bounding sphere API to add center.

--- SparrowMesh/Sources/SparrowMesh/Structure/SPMSphereBounds.swift
+++ SparrowMesh/Sources/SparrowMesh/Structure/SPMSphereBounds.swift
@@ -10,26 +10,41 @@ import simd
 
 /// Spherical bounds centered at origin.
 public struct SPMSphereBounds: BinaryCodable {
+    /// Sphere center
+    public let center: SIMD3<Float>
+    
     /// Radius of the sphere
     public let radius: Float
     
     /// Empty spherical bounds
     public init() {
+        self.center = [0, 0, 0]
         self.radius = 0
     }
     
     /// Spherical bounds with given radius
-    public init(radius: Float) {
+    public init(center: SIMD3<Float>, radius: Float) {
+        self.center = center
         self.radius = radius
     }
     
-    /// Create a new bounds containing this and the given bounds.
-    public func containing(_ other: SPMSphereBounds) -> SPMSphereBounds {
-        return SPMSphereBounds(radius: max(self.radius, other.radius))
+    public init(box: SPMBounds) {
+        self.center = (box.min + box.max) / 2
+        
+        self.radius = max(
+            length(self.center, )
+        )
     }
     
-    /// Create a new bounds containing this and the the given point
-    public func containing(_ other: SIMD3<Float>) -> SPMBounds {
-        return SPMSphereBounds(radius: max(self.radius, length(other)))
+    /// Create a new bounds containing all given spheres
+    public static func containing(_ others: [SPMSphereBounds]) -> SPMSphereBounds {
+        // TODO: how?
+        return SPMSphereBounds()
+    }
+
+    /// Create a new bounds containing all given points
+    public static func containing(_ points: [SIMD3<Float>]) -> SPMSphereBounds {
+        // TODO https://en.wikipedia.org/wiki/Bounding_sphere
+        return SPMSphereBounds()
     }
 }
